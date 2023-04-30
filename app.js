d1e9396b9dd7c38505ed2301e2a7debab95453d5
jasmin <jasminchakma895@gmail.com>
2023-04-30 20:42:10 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var watch = document.querySelector(".watch");
+var Day = document.querySelector(".day");
+ var Todays = ["sunday", "monday", "tuesday", "wednessday", "thusday", "friday", "satarday"];
+setInterval(function(){
+
+    var date = new Date ();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    hours = hours % 12;
+    minutes = check(minutes);
+    seconds = check (seconds);
+    var time = hours + ": " + minutes + ":" + seconds;
+    watch.textContent = time;
+
+},1000)
+
+function check(e){
+    if(e<10){
+        e = "0" +e;
+    }return e;
+};
