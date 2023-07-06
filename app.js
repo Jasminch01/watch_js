725eec7168de8baa0b37a4c14a44d9477e847380
jasmin <jasminchakma895@gmail.com>
2023-07-06 12:07:40 +0600

    update watch function

--- app.js
+++ app.js
@@ -1,57 +1,60 @@
-// var watch = document.querySelector(".watch");
-// setInterval(function(){
-
-//     var date = new Date ();
-//     var hours = date.getHours();
-//     var minutes = date.getMinutes();
-//     var seconds = date.getSeconds();
-//     hours = hours % 12;
-//     minutes = check(minutes);
-//     seconds = check (seconds);
-//     var time = hours + ": " + minutes + ":" + seconds;
-//     watch.textContent = time;
-
-// },1000)
-
-// function check(e){
-//     if(e<10){
-//         e = "0" +e;
-//     }return e;
-// };
-var watch = document.querySelector(".watch");
-var Day = ["sunday", "satarday", "monday", "tuesday", "wednessday", "thusday", "friday"]
-var ampm = document.querySelector(".ampm");
-
+let todaysDate = document.querySelector(".date");
+let hour = document.querySelector(".hour");
+let minute = document.querySelector(".minute");
+let second = document.querySelector(".second")
+let ampm = document.querySelector(".ampm");
+let loader = document.querySelector (".loader");
+let today = document.querySelector (".today");
+let days = ['Sunday', 'Monday', "Tuesday", 'Wednesday', "Thursday", 'Friday', "Saturday"];
 
 setInterval(() => {
-   var date = new Date();
+   let date = new Date();
 
-var today = Day[date.getDay()];
-var hour= date.getHours();
-var minutes = date.getMinutes();
-var second = date.getSeconds();
-hour = hour %12;
-minutes = count(minutes);
-second = count (second);
 
-var time = hour + ":" + minutes + ":" + second+ " " + today;
+   //time 
+   let currentHour = date.getHours();
+   let currentMinute = date.getMinutes();
+   let currentSecond = date.getSeconds();
+   today.innerText = days[date.getDay()];
+   todaysDate.innerText = date.toLocaleDateString();
 
-watch.textContent = time;
 
-if (hour >= 0 && hour < 12) {
-   ampm.innerText = 'AM'
-} else if (hour > 11 && hour < 24) {
-   ampm.innerText = 'PM'
-} else {
-   ampm.innerText = 'AM'
-}
-   
+   //hour 
+
+   if(currentHour >= 22) {
+       hour.innerText = `${currentHour -12}`;
+   }else if (currentHour >12 && currentHour <22){
+       hour.innerText = `0${currentHour - 12}`
+   }else if (currentHour >=10 && currentHour <=22){
+       hour.innerText = `${currentHour}`
+   }else if (currentHour < 10){
+       hour.innerText = `0${currentHour}`
+   }
+
+   if (currentMinute < 10) {
+       minute.innerText = `0${currentMinute}`;
+   } else {
+       minute.innerText = `${currentMinute}`;
+   }
+
+   //second 
+
+   if (currentSecond < 10) {
+       loader.style.width = `${(100/60)* currentSecond}%`
+       second.innerText = `0${currentSecond}`
+       
+   }else {
+       loader.style.width = `${(100/60)* currentSecond}%`
+       second.innerText = `${currentSecond}`;
+   }
+
+   //ampm 
+   if(currentHour >= 0 && currentHour <12) {
+       ampm.innerText = "AM";
+   } else if (currentHour >11 && currentHour <24){
+       ampm.innerText = "PM";
+   } else{
+       ampm.innerText = "AM"
+   }
+
 }, 1000);
-
-function count (e){
-   if(e<10){
-      e = "0"+ e;
-   }return e;
-}
-
-count();
