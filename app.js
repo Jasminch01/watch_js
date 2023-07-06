7a9ca904acacf22620bf00d3ad297108dbf40642
jasmin <jasminchakma895@gmail.com>
2023-07-06 12:01:44 +0600

    change markup

--- app.js
+++ app.js
@@ -1,20 +1,57 @@
+// var watch = document.querySelector(".watch");
+// setInterval(function(){
+
+//     var date = new Date ();
+//     var hours = date.getHours();
+//     var minutes = date.getMinutes();
+//     var seconds = date.getSeconds();
+//     hours = hours % 12;
+//     minutes = check(minutes);
+//     seconds = check (seconds);
+//     var time = hours + ": " + minutes + ":" + seconds;
+//     watch.textContent = time;
+
+// },1000)
+
+// function check(e){
+//     if(e<10){
+//         e = "0" +e;
+//     }return e;
+// };
 var watch = document.querySelector(".watch");
-setInterval(function(){
+var Day = ["sunday", "satarday", "monday", "tuesday", "wednessday", "thusday", "friday"]
+var ampm = document.querySelector(".ampm");
 
-    var date = new Date ();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
-    hours = hours % 12;
-    minutes = check(minutes);
-    seconds = check (seconds);
-    var time = hours + ": " + minutes + ":" + seconds;
-    watch.textContent = time;
 
-},1000)
+setInterval(() => {
+   var date = new Date();
 
-function check(e){
-    if(e<10){
-        e = "0" +e;
-    }return e;
-};
+var today = Day[date.getDay()];
+var hour= date.getHours();
+var minutes = date.getMinutes();
+var second = date.getSeconds();
+hour = hour %12;
+minutes = count(minutes);
+second = count (second);
+
+var time = hour + ":" + minutes + ":" + second+ " " + today;
+
+watch.textContent = time;
+
+if (hour >= 0 && hour < 12) {
+   ampm.innerText = 'AM'
+} else if (hour > 11 && hour < 24) {
+   ampm.innerText = 'PM'
+} else {
+   ampm.innerText = 'AM'
+}
+   
+}, 1000);
+
+function count (e){
+   if(e<10){
+      e = "0"+ e;
+   }return e;
+}
+
+count();
